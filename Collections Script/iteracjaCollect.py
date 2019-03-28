0c09329ea420da0aae7db82ec5308bef1e694843
ulalu <lousalome23@gmail.com>
2019-03-28 18:18:18 +0100

    2803 lil

--- Collections Script/iteracjaCollect.py
+++ Collections Script/iteracjaCollect.py
@@ -7,3 +7,17 @@ di={"name": 'Ann',"age":22,'pet':"cat"}
 
 for key,va in di.items():
     print("%s --->>> %s " %(key,va))
+
+
+########
+x=0
+y=True
+while x<=5 and y:  # and false or true warunek
+    print("yesss")
+    x+=1
+    y=bool(x<4)
+
+#### Dictionery and for
+
+for v in di:  # default keys
+    print(v)
