0ea56b2122dedf1865cb5e0a17825f22dbc32d66
louAphasia <42347224+louAphasia@users.noreply.github.com>
2019-03-29 18:09:34 +0100

    collect it 2803

--- Collections Script/iteracjaCollect.py
+++ Collections Script/iteracjaCollect.py
@@ -17,7 +17,37 @@ while x<=5 and y:  # and false or true warunek
     x+=1
     y=bool(x<4)
 
+z=False
+m=5
+while m>3 and not z:
+   print("nooo")
+   m-=1
+   z=bool(m<3)
+
 #### Dictionery and for
 
-for v in di:  # default keys
+for v in {"ta":23,"ert":2344}:  # default keys
     print(v)
+
+for tuple in (2,'345', True):
+    print(tuple)
+
+### for many append for *2
+
+li=["cat", 'dog', 'bird']
+a=[]
+
+for m in li:
+    for xx  in m:
+        a.append(xx)
+print(a)
+
+#### for range
+
+
+for e in range(-1,10,3):
+    print(e)
+
+
+sss=[ch.upper() for ch in 'comph' if ch not in 'aeiou']
+print(sss)
