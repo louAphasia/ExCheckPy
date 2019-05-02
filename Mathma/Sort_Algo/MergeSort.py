dfb04f8b241024744d706d6eaac61218c207b024
louAphasia <lousalome23@gmail.com>
2019-05-02 21:09:52 +0200

    ibm sortalgo3

--- Mathma/Sort_Algo/MergeSort.py
+++ Mathma/Sort_Algo/MergeSort.py
@@ -1,7 +1,4 @@
-#error
-
-
-"""def mergesort(arr):
+def mergesort(arr):
     print("splitting", arr)
     if len(arr)>1:
         mid=len(arr)//2
@@ -34,37 +31,8 @@
             j=j+1
             k=k+1
 
-        print("mergin",arr)"""
+        print("mergin",arr)
 
-array=[23,0,3,12,1,6,20]
+array=[23,0,3,22,111,12,21]
 
-#mergesort(array)
-
-
-def mergeso(arr):
-    if len(arr)<=1:
-        return arr
-    mid=len(arr)//2
-    left,right=mergeso(arr[:mid]),mergeso(arr[mid:])
-
-    return merge(left,right,arr.copy())
-
-def merge(left,right,merged):
-    leftcur,rightcur=0,0
-    while leftcur<len(left) and rightcur<len(right):
-        if left[leftcur]<=right[rightcur]:
-            merged[leftcur+rightcur]=left[leftcur]
-            leftcur+=1
-        else:
-            merged[leftcur+rightcur]=right[rightcur]
-            rightcur+=1
-
-    for leftcur in range(leftcur,len(left)):
-        merged[leftcur + rightcur] = left[leftcur]
-
-    for rightcur in range(rightcur,len(right)):
-        merged[leftcur + rightcur] = right[rightcur]
-    return merged
-
-
-print(mergeso(array))
+mergesort(array)
