89ce839d843ad3237a8ccb620f57e480100ad699
louAphasia <42347224+louAphasia@users.noreply.github.com>
2019-05-03 11:29:06 +0200

    z jogi merge NEW

--- Mathma/Sort_Algo/MergeSort.py
+++ Mathma/Sort_Algo/MergeSort.py
@@ -1,38 +1,35 @@
-def mergesort(arr):
-    print("splitting", arr)
-    if len(arr)>1:
-        mid=len(arr)//2
-        lefthalf=arr[:mid]
-        righthalf=arr[mid:]
+array1=[23,0,211,22,21,0,4]
 
-        mergesort(lefthalf)
-        mergesort(righthalf)
 
-        i=0
-        j=0
-        k=0
+array=[23,0,3,12,1,6,20]
 
-        while i<len(lefthalf) and j<len(righthalf):
-            if lefthalf[i]<righthalf[i]:
-                arr[k]=lefthalf[i]
-                i=i+1
-            else:
-                arr[k]=righthalf[j]
-                j=j+1
-            k=k+1
+#mergesort(array)
 
-        while i<len(lefthalf):
-            arr[k]=lefthalf[i]
-            i=i+1
-            k=k+1
 
-        while j<len(righthalf):
-            arr[k]=righthalf[j]
-            j=j+1
-            k=k+1
+def mergeso(arr):
+    if len(arr)<=1:
+        return arr
+    mid=len(arr)//2
+    left,right=mergeso(arr[:mid]),mergeso(arr[mid:])
 
-        print("mergin",arr)
+    return merge(left,right,arr.copy())
 
-array=[23,0,3,22,111,12,21]
+def merge(left,right,merged):
+    leftcur,rightcur=0,0
+    while leftcur<len(left) and rightcur<len(right):
+        if left[leftcur]<=right[rightcur]:
+            merged[leftcur+rightcur]=left[leftcur]
+            leftcur+=1
+        else:
+            merged[leftcur+rightcur]=right[rightcur]
+            rightcur+=1
 
-mergesort(array)
+    for leftcur in range(leftcur,len(left)):
+        merged[leftcur + rightcur] = left[leftcur]
+
+    for rightcur in range(rightcur,len(right)):
+        merged[leftcur + rightcur] = right[rightcur]
+    return merged
+
+
+print(mergeso(array))
