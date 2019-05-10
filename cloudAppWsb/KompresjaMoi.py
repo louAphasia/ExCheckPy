7acac75f7d5e3265c0bd6bcf02f7b33edda8519a
louAphasia <42347224+louAphasia@users.noreply.github.com>
2019-05-10 20:59:06 +0200

    joga kompmoi komp bitowe

--- cloudAppWsb/KompresjaMoi.py
+++ cloudAppWsb/KompresjaMoi.py
@@ -1,6 +1,6 @@
 import math
 
-s="aabbccdd"
+s="aabbccddee"
 
 def slownik(t):
     s=sorted(set(t))
@@ -27,14 +27,42 @@ def bin(s):
     return str(s) if s<=1 else bin(s>>1) + str(s&1)
 
 
+def lentext(wejscie):
+    return len(wejscie)
 
-def listabitow(dlugosc):
+def nadmiarowe(liczbabitnaznak,lentext):
+    dlugoscbit=math.ceil(((liczbabitnaznak*lentext)+3)/8)*8
+    nadmiar=dlugoscbit-(3+liczbabitnaznak*lentext)
+    return nadmiar
+
+print(nadmiarowe(bitnaznak(dlugosc(slownik(s))),lentext(s)))
+
+def listabitow(dlugosc,b):
     lista=[]
     for i in range(dlugosc):
-        lista.append("%02d" % (int(bin(i))))
+        lista.append(f"%0{b}d" % (int(bin(i))))
     return lista
 
-print(listabitow(dlugosc(slownik(s))))
+#lista z bitami
+lsb=listabitow(dlugosc(slownik(s)),bitnaznak(dlugosc(slownik(s))))
+slo=list(slownik(s))
+#slownik znak : bity
+Dict=dict(zip(slo,lsb))
+print(Dict)
 d=1
 p=3*0
 print(f"%0{4}d" %(d))
+
+def ilebitend(nadmiarowe):
+    end=''
+    for i in range(nadmiarowe):
+         end+='1'
+    return end
+
+print(ilebitend(nadmiarowe(bitnaznak(dlugosc(slownik(s))),lentext(s))))
+
+def ilebitFirst(nadmiarowe):
+    dicto={0:'000',1:'001',2:'010',3:'011',4:'100',5:'101',6:'110',7:'111'}
+    return dicto.get(nadmiarowe)
+
+print(ilebitFirst(nadmiarowe(bitnaznak(dlugosc(slownik(s))),lentext(s))))
