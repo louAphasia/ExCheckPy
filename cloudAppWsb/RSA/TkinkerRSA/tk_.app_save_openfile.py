11aca683f694295c98e71a90d167730a7d7ccd13
ulalu <lousalome23@gmail.com>
2019-05-30 12:17:41 +0200

    z little tkinker calculate

--- cloudAppWsb/RSA/TkinkerRSA/tk_.app_save_openfile.py
+++ cloudAppWsb/RSA/TkinkerRSA/tk_.app_save_openfile.py
@@ -8,8 +8,7 @@ import tkinter as tk
 # funkcje £ szyfrujaca i druga zapisujaca do pliku
 def calculate(*args):
     try:
-        value=float(feet.get())
-        meters.set((0.3*value*10000+0.5)/10000)
+        value.set(accounts)
     except ValueError:
         pass
 
@@ -18,7 +17,7 @@ def saveasfile():
     savea=filedialog.asksaveasfile(mode='w', defaultextension='.txt')
 
     if savea:
-        texttosave=feeten.get()
+        texttosave=feeten2.get()
         savea.write(texttosave)
         savea.close()
     else:
@@ -30,13 +29,37 @@ def openfile():
     filename=filedialog.askopenfilename(initialdir="/",title="Open file",filetypes=(("Text files", "*.txt"),("All Files", "*.*")))
 
 
+def database():
+    with open("wpis.txt", "r") as f:
+        return f.read()
+
+
+accounts = database()
+'''
+def confirm():
+    u, p = usernameB.get(), passwordB.get()
+    if accounts.get(u) == p:
+        print(True)
+        # do whatever here
+    else:
+        print(False)
+        #  login is bad
+        m  = messagebox.askretrycancel("Invalid input")
+        if not m:
+             master.quit()'''
+
 #### funkcja save do pliku
 def save():
     filename='wpis.txt'
     if filename:
-        texta=feeten.get()
+        data1=feeten1.get()
+
+        data2=feeten2.get()
+        data3=feeten3.get()
         savet=open(filename,'a')
-        savet.write(texta)
+        savet.write(data1)   # musi pionowo z przerwa
+        savet.write(data2)
+        savet.write(data3)
         savet.close()
     else:
         messagebox.showinfo("Error", "no file open")
@@ -52,40 +75,34 @@ mainframe.grid(column=0,row=0,sticky=(N,W,E,S))
 mainframe.columnconfigure(0,weight=1)
 mainframe.rowconfigure(0,weight=1)
 
-
 #feet do funkcji
-feet=StringVar()
-meters=StringVar()
-feet1=StringVar()
+surname=StringVar()
+value=StringVar()
+name=StringVar()
 pesel=IntVar()
 
 # FEET ENTRY
 label_surname=ttk.Label(mainframe,text='NAZWISKO').grid(column=1,row=1,sticky=(W,E))
-feeten1=ttk.Entry(mainframe, textvariable=feet)
+feeten1=ttk.Entry(mainframe,width=40, textvariable=surname)
 feeten1.grid(column=2,row=1,sticky=(N,S),columnspan=2)
 
 label_name=ttk.Label(mainframe,text='IMIE').grid(column=1,row=2,sticky=(W,E))
-feeten2=ttk.Entry(mainframe, textvariable=feet1)
+feeten2=ttk.Entry(mainframe,width=40, textvariable=name)
 feeten2.grid(column=2,row=2,sticky=(N,S),columnspan=2)
 
 label_pesel=ttk.Label(mainframe,text='PESEL').grid(column=1,row=3,sticky=(W,E))
-feeten3=ttk.Entry(mainframe, textvariable=pesel)
+feeten3=ttk.Entry(mainframe,width=40, textvariable=pesel)
 feeten3.grid(column=2,row=3,sticky=(N,S),columnspan=2)
 # LABEL  Button w MAINFRAME
 
 label_rekordy=ttk.Label(mainframe,text="liczba wpisanych Rekordów").grid(column=4,row=1,sticky=(W,E))
-label_rekordyCount=ttk.Label(mainframe,textvariable=meters).grid(column=4,row=2,sticky=(W,E))
+label_rekordyCount=ttk.Label(mainframe,width=50,textvariable=value).grid(column=4,row=2,sticky=(W,E))
 
-ttk.Button(mainframe,text="Dodaj",command=calculate).grid(column=2,row=4,sticky=W)
+ttk.Button(mainframe,text="Dodaj",command=save).grid(column=2,row=4,sticky=W)
 
-ttk.Button(mainframe,text="Szyfruj i przeslij",command=calculate).grid(column=3,row=4,sticky=W)
+ttk.Button(mainframe,text="Szyfruj i przeslij",command=database).grid(column=3,row=4,sticky=W)
 
-
-
-
-
-
-tk.Button(mainframe,text="pokaż ilosc rekordow",relief="raised").grid(column=4,row=4)
+tk.Button(mainframe,text="pokaż ilosc rekordow",relief="raised",command=calculate).grid(column=4,row=4)
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5,pady=5)
